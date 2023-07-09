1459059bb5b166f27236b5aee72bce810efa52f1
Collin Chiu <61135705+cchiu727@users.noreply.github.com>
2023-07-08 23:20:17 -0700

    Reorganize index.js code

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-function generateHex() {
-    return '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-}
-
 const button = document.getElementById('color-button');
 const background = document.getElementById('background-container');
 const colorLabel = document.getElementById('color-label');
 
+function generateHex() {
+    return '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+}
 
 button.addEventListener("click", function() {
     let color = generateHex().toUpperCase();
